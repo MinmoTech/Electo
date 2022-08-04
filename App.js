9862aaa4dc5a8f3c80ac00ee610cadf8695441dd
Minmo <maroboro.mike@gmail.com>
2022-08-04 15:40:51 +0200

    temp

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import ShakaPlayer from 'shaka-player-react';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <Text>Open up App.js to start working on your app!</Text>
+     <ShakaPlayer autoPlay src="http://ftp.itec.aau.at/datasets/mmsys12/TheSwissAccount/MPDs/TheSwissAccount_15s_isoffmain_DIS_23009_1_v_2_1c2_2011_08_30.mpd" />
+
+    <StatusBar style="auto" />
     </View>
   );
 }
